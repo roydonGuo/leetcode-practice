186b05863a1b66c3d3b7c6d1571132aadb25bd9b
roydonGuo <3133010060@qq.com>
2023-09-19 16:29:52 +0800

    getter

--- src/main/java/com/roydon/domain/ListNode.java
+++ src/main/java/com/roydon/domain/ListNode.java
@@ -18,4 +18,20 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
 }
