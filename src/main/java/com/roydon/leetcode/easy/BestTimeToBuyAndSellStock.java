fcfb8f2c175aa70c6c741fba49bdddc853b914d4
roydonGuo <3133010060@qq.com>
2023-10-01 13:26:50 +0800

    121. 买卖股票的最佳时机

---
+++ src/main/java/com/roydon/leetcode/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,27 @@
+package com.roydon.leetcode.easy;
+
+/**
+ * BestTimeToBuyAndSellStock
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/10/1
+ * 121. 买卖股票的最佳时机
+ **/
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int length = prices.length;
+        int min = prices[0], max = 0;
+        for (int i = 0; i < length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else if (prices[i] - min > max) {
+                max = prices[i];
+            }
+        }
+        return max;
+    }
+}
